4495fed73cd6248ae605683ab5dd49b306c97d50
Christoph Dlapa <dlapac@pcx40209.desy.de>
2022-09-06 14:49:06 +0200

    Deleted integral eight in the initial basis of the two-loop non-planar three-point integrals. Added 4PM potential example

---
+++ examples/elliptic_4PM_potential_integrals/TMatrix_4PM_pot_to_UT.m
@@ -0,0 +1,60 @@
+{{((3*x)/16 - (5*eps*x)/8 - (93*eps^2*x)/4 + (245*eps^3*x)/2 - 150*eps^4*x + 
+    (3*x^3)/8 - (11*eps*x^3)/4 - (47*eps^2*x^3)/2 + 155*eps^3*x^3 - 
+    200*eps^4*x^3 + (3*x^5)/16 - (5*eps*x^5)/8 - (93*eps^2*x^5)/4 + 
+    (245*eps^3*x^5)/2 - 150*eps^4*x^5)/(eps^4 - eps^4*x^2 - eps^4*x^4 + 
+    eps^4*x^6), (-2*x*dpsi[1][x]*psi[1][x] + 2*x^5*dpsi[1][x]*psi[1][x] - 
+    psi[1][x]^2/2 - (8*eps*psi[1][x]^2)/3 - x^2*psi[1][x]^2 - 
+    (64*eps*x^2*psi[1][x]^2)/3 + (3*x^4*psi[1][x]^2)/2 - 
+    (8*eps*x^4*psi[1][x]^2)/3)/(eps + eps*x^2), 
+  (1/4 + eps + x^2/2 + x^4/4 + eps*x^4)/(eps^2*x + eps^2*x^3), 
+  (3*x + 6*eps*x)/(eps^2 + eps^2*x^2)}, {0, 1, 0, 0}, 
+ {((-3*x^2*dpsi[1][x]*psi[1][x])/8 + (5*eps*x^2*dpsi[1][x]*psi[1][x])/4 + 
+    (93*eps^2*x^2*dpsi[1][x]*psi[1][x])/2 - 245*eps^3*x^2*dpsi[1][x]*
+     psi[1][x] + 300*eps^4*x^2*dpsi[1][x]*psi[1][x] - 
+    (3*x^4*dpsi[1][x]*psi[1][x])/8 + (17*eps*x^4*dpsi[1][x]*psi[1][x])/4 + 
+    (eps^2*x^4*dpsi[1][x]*psi[1][x])/2 - 65*eps^3*x^4*dpsi[1][x]*psi[1][x] + 
+    100*eps^4*x^4*dpsi[1][x]*psi[1][x] + (3*x^6*dpsi[1][x]*psi[1][x])/8 - 
+    (17*eps*x^6*dpsi[1][x]*psi[1][x])/4 - (eps^2*x^6*dpsi[1][x]*psi[1][x])/
+     2 + 65*eps^3*x^6*dpsi[1][x]*psi[1][x] - 100*eps^4*x^6*dpsi[1][x]*
+     psi[1][x] + (3*x^8*dpsi[1][x]*psi[1][x])/8 - 
+    (5*eps*x^8*dpsi[1][x]*psi[1][x])/4 - (93*eps^2*x^8*dpsi[1][x]*psi[1][x])/
+     2 + 245*eps^3*x^8*dpsi[1][x]*psi[1][x] - 300*eps^4*x^8*dpsi[1][x]*
+     psi[1][x] + (eps*x*psi[1][x]^2)/8 + (31*eps^2*x*psi[1][x]^2)/12 - 
+    (123*eps^3*x*psi[1][x]^2)/2 + (785*eps^4*x*psi[1][x]^2)/3 - 
+    300*eps^5*x*psi[1][x]^2 - (3*x^3*psi[1][x]^2)/8 - 
+    (11*eps*x^3*psi[1][x]^2)/8 + (169*eps^2*x^3*psi[1][x]^2)/4 + 
+    (1679*eps^3*x^3*psi[1][x]^2)/6 - 2195*eps^4*x^3*psi[1][x]^2 + 
+    (8900*eps^5*x^3*psi[1][x]^2)/3 - (45*eps*x^5*psi[1][x]^2)/8 + 
+    (167*eps^2*x^5*psi[1][x]^2)/4 + (2069*eps^3*x^5*psi[1][x]^2)/6 - 
+    2295*eps^4*x^5*psi[1][x]^2 + (8900*eps^5*x^5*psi[1][x]^2)/3 + 
+    (3*x^7*psi[1][x]^2)/8 - (9*eps*x^7*psi[1][x]^2)/8 - 
+    (527*eps^2*x^7*psi[1][x]^2)/12 + (367*eps^3*x^7*psi[1][x]^2)/2 - 
+    (115*eps^4*x^7*psi[1][x]^2)/3 - 300*eps^5*x^7*psi[1][x]^2)/
+   (eps^5 - eps^5*x^2 - eps^5*x^4 + eps^5*x^6), 
+  (2*x^2*dpsi[1][x]^2*psi[1][x]^2 - 2*x^4*dpsi[1][x]^2*psi[1][x]^2 - 
+    2*x^6*dpsi[1][x]^2*psi[1][x]^2 + 2*x^8*dpsi[1][x]^2*psi[1][x]^2 + 
+    x*dpsi[1][x]*psi[1][x]^3 + (16*eps*x*dpsi[1][x]*psi[1][x]^3)/3 + 
+    x^3*dpsi[1][x]*psi[1][x]^3 + (112*eps*x^3*dpsi[1][x]*psi[1][x]^3)/3 - 
+    5*x^5*dpsi[1][x]*psi[1][x]^3 - (112*eps*x^5*dpsi[1][x]*psi[1][x]^3)/3 + 
+    3*x^7*dpsi[1][x]*psi[1][x]^3 - (16*eps*x^7*dpsi[1][x]*psi[1][x]^3)/3 - 
+    (eps*psi[1][x]^4)/3 - (14*eps^2*psi[1][x]^4)/9 + x^2*psi[1][x]^4 + 
+    (65*eps*x^2*psi[1][x]^4)/3 + (314*eps^2*x^2*psi[1][x]^4)/3 - 
+    2*x^4*psi[1][x]^4 - (47*eps*x^4*psi[1][x]^4)/3 + 
+    (314*eps^2*x^4*psi[1][x]^4)/3 + x^6*psi[1][x]^4 - 
+    (17*eps*x^6*psi[1][x]^4)/3 - (14*eps^2*x^6*psi[1][x]^4)/9)/
+   (eps^2 + eps^2*x^2), (-1/2*(x*dpsi[1][x]*psi[1][x]) - 
+    2*eps*x*dpsi[1][x]*psi[1][x] - (x^3*dpsi[1][x]*psi[1][x])/2 + 
+    2*eps*x^3*dpsi[1][x]*psi[1][x] + (x^5*dpsi[1][x]*psi[1][x])/2 - 
+    2*eps*x^5*dpsi[1][x]*psi[1][x] + (x^7*dpsi[1][x]*psi[1][x])/2 + 
+    2*eps*x^7*dpsi[1][x]*psi[1][x] + (eps*psi[1][x]^2)/6 + 
+    (2*eps^2*psi[1][x]^2)/3 - (x^2*psi[1][x]^2)/2 - 
+    (11*eps*x^2*psi[1][x]^2)/2 - (34*eps^2*x^2*psi[1][x]^2)/3 - 
+    (15*eps*x^4*psi[1][x]^2)/2 - (34*eps^2*x^4*psi[1][x]^2)/3 + 
+    (x^6*psi[1][x]^2)/2 + (13*eps*x^6*psi[1][x]^2)/6 + 
+    (2*eps^2*x^6*psi[1][x]^2)/3)/(eps^3*x + eps^3*x^3), 
+  (-6*x^2*dpsi[1][x]*psi[1][x] - 12*eps*x^2*dpsi[1][x]*psi[1][x] + 
+    6*x^4*dpsi[1][x]*psi[1][x] + 12*eps*x^4*dpsi[1][x]*psi[1][x] - 
+    3*x*psi[1][x]^2 - 22*eps*x*psi[1][x]^2 - 32*eps^2*x*psi[1][x]^2 + 
+    3*x^3*psi[1][x]^2 - 10*eps*x^3*psi[1][x]^2 - 32*eps^2*x^3*psi[1][x]^2)/
+   (eps^3 + eps^3*x^2)}, {(12*x - 184*eps*x + 720*eps^2*x - 800*eps^3*x)/
+   (eps^3 - eps^3*x^2), 0, 0, 0}}
